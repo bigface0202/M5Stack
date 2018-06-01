5ab2afa568a6d79104b98dfc2a84f4748a78344d
Unknown <theshoreunder@gmail.com>
2018-06-01 15:30:56 +0900

    Add IRrecv

--- M5Stack_music_wav/src/main.cpp
+++ M5Stack_music_wav/src/main.cpp
@@ -16,9 +16,10 @@ https://github.com/me-no-dev/arduino-esp32fs-plugin
 #include "AudioOutputI2S.h"
 #include "HTTPClient.h"
 #include "SPIFFS.h"
-
+/*Pin*/
 int switch_PIN = 2; //SHOT switch
-/*Pin number 3 is IR*/
+int IR_receptorPin = 5;//Pin used to read IR values
+
 int switchOut;
 int shootCount = 0;
 int lifeCount = 0;
@@ -28,9 +29,53 @@ int i = 0;
 AudioGeneratorWAV *wav;
 AudioFileSourceSD *file_shoot;
 AudioOutputI2S *out;
+/*IR*/
+IRsend irsend; //Pin number 3 is IR
+IRrecv irrecv(IR_receptorPin);//Create an object
+decode_results results;
 
-//IR shoot
-IRsend irsend; //create a IRsend object just apply for pin 9 in ATMega328
+//Function
+void IR_Receptor();
+void IR_Transmitter();
+
+void IR_Receptor() {
+  if (irrecv.decode(&results)) {
+    if (results.decode_type == SONY) {
+      lifeCount++;
+      M5.Lcd.fillCircle(180-(25*lifeCount),55,10,BLACK);
+    }
+    irrecv.resume(); // Receive the next value
+  }
+}
+
+void IR_Transmitter() {
+  /*Shoot switch was pressed*/
+  if (switchOut == 1){
+    irsend.sendSony(0xa90, 12);
+
+    M5.Lcd.setTextColor(RED); 
+    M5.Lcd.setCursor(0, 220);
+    M5.Lcd.print("SHOOT!!!");
+    
+    //Overwrite the SHOOT!!!! and shootCount
+    M5.Lcd.setTextColor(BLACK);
+    M5.Lcd.setCursor(80, 10);
+    M5.Lcd.print(shootCount);
+    M5.Lcd.setCursor(0, 220);
+    M5.Lcd.print("SHOOT!!!");
+    
+    M5.Lcd.setTextColor(WHITE);
+    shootCount = shootCount + 5;
+    //file_shoot->close();
+    //file_shoot = new AudioFileSourceSD("/se_maoudamashii_battle_gun05.wav");
+    //wav->begin(file_shoot, out);
+    /*Show your shots*/
+    M5.Lcd.setCursor(0, 10);
+    M5.Lcd.print("SHOT:");
+    M5.Lcd.setCursor(80, 10);
+    M5.Lcd.print(shootCount);
+  }
+}
 
 void setup()
 {
@@ -43,7 +88,6 @@ void setup()
   /*Please move music file(se_maoudamashii_battle_gun05.wav) into SD.
   This file put on the music folder*/
   file_shoot = new AudioFileSourceSD("/se_maoudamashii_battle_gun05.wav");
-  //file = new AudioFileSourceSD("/pno-cs.wav");
   out = new AudioOutputI2S(0, 1); // Output to builtInDAC
   out->SetOutputModeMono(true);
   wav = new AudioGeneratorWAV();
@@ -53,24 +97,11 @@ void setup()
   pinMode(switch_PIN, INPUT);
   M5.Lcd.setTextFont(4);
   M5.Lcd.setTextColor(WHITE);
-  M5.Lcd.setCursor(0, 200);
+  M5.Lcd.setCursor(160, 120);
   M5.Lcd.print("START!!");
   delay(2000);
   M5.Lcd.fillScreen(BLACK);
-}
-
-void loop()
-{
-  /*LCD setup*/
-  M5.Lcd.setTextFont(4);
-  M5.Lcd.drawRect(120,100,200,140,WHITE);
-
-  /*Show your shots*/
-  M5.Lcd.setCursor(0, 10);
-  M5.Lcd.print("SHOT:");
-  M5.Lcd.setCursor(80, 10);
-  M5.Lcd.print(shootCount);
-
+  
   /*Show your live*/
   M5.Lcd.setCursor(0, 45);
   M5.Lcd.print("LIFE:");
@@ -82,6 +113,27 @@ void loop()
   M5.Lcd.fillCircle(155,55,10,RED);
   M5.Lcd.fillCircle(180,55,10,RED);
 
+  /*Show your shots*/
+  M5.Lcd.setCursor(0, 10);
+  M5.Lcd.print("SHOT:");
+  M5.Lcd.setCursor(80, 10);
+  M5.Lcd.print(shootCount);
+}
+
+void loop()
+{
+  /*LCD setup*/
+  M5.Lcd.setTextFont(4);
+  M5.Lcd.drawRect(120,100,200,140,WHITE);
+
+  /*Read switch*/
+  switchOut = digitalRead(switch_PIN);
+
+  /*IR Receptor*/
+  IR_Receptor();
+
+  /*IR Transmitter*/
+  IR_Transmitter();
 
   /*??*/
   if(wav->isRunning()){
@@ -90,35 +142,14 @@ void loop()
     }
   }
 
-  /*Read switch*/
-  switchOut = digitalRead(switch_PIN);
-
-  /*Shoot switch was pressed*/
-  if (switchOut == 1){
-    M5.Lcd.setTextColor(RED); 
-    M5.Lcd.setCursor(0, 220);
-    M5.Lcd.print("SHOOT!!!");
-    
-    M5.Lcd.setTextColor(BLACK);
-    M5.Lcd.setCursor(80, 10);
-    M5.Lcd.print(shootCount);
-    
-    M5.Lcd.setTextColor(WHITE);
-    shootCount = shootCount + 5;
-    irsend.sendSony(0xa90, 12);
+  if (M5.BtnB.wasPressed()){
+    /*Music test*/
     //file_shoot->close();
     //file_shoot = new AudioFileSourceSD("/se_maoudamashii_battle_gun05.wav");
     //wav->begin(file_shoot, out);
-    M5.Lcd.fillScreen(BLACK);
-
-  }else if (M5.BtnB.wasPressed()){
-    /*Music test*/
-    file_shoot->close();
-    file_shoot = new AudioFileSourceSD("/se_maoudamashii_battle_gun05.wav");
-    wav->begin(file_shoot, out);
     dacWrite(25, 0);
   }else if (M5.BtnC.wasPressed()){
-    
+    /*Showing like the heart beat*/
     while(heartCount < 3){
       M5.Lcd.drawLine(140,170,170,170,RED);
       M5.Lcd.drawLine(170,170,200,110,RED);
