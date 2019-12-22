842d051b15688bb7ce3a8b6d22365fcd120d35cb
Yusuke Kishishita <31816350+bigface0202@users.noreply.github.com>
2019-12-22 11:11:37 +0900

    update

--- M5StickC_terrarium_right/src/main.cpp
+++ M5StickC_terrarium_right/src/main.cpp
@@ -17,6 +17,7 @@ void setup() {
 }
 
 int num = 0;
+int phase = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -24,14 +25,42 @@ void loop() {
   M5.Lcd.setCursor(0, 0);
   if(M5.BtnA.wasPressed()){
     num++;
-    if(num > 5){
+    if(num > 6){
       num = 0;
     }
   }
   if(num == 0){
-    M5.Lcd.println("OFF   ");
-    for(int i = 0; i < 7; i++){
-      leds[i] = CRGB::Black;
+    M5.Lcd.println("Loop  ");
+      if (phase == 0){
+        for(int i = 0; i < 7; i++){
+          if (i % 2 == 0){
+            leds[i] = CRGB::Red;
+          }else if (i % 2 == 1){
+            leds[i] = CRGB::Blue;
+          }
+        }
+      delay(1000);
+      }else if (phase == 1){
+        for(int i = 0; i < 7; i++){
+          if (i % 2 == 0){
+            leds[i] = CRGB::Blue;
+          }else if (i % 2 == 1){
+            leds[i] = CRGB::Green;
+          }
+        }
+      delay(1000);
+      }else if (phase == 2){
+        for(int i = 0; i < 7; i++){
+          if (i % 2 == 0){
+            leds[i] = CRGB::Green;
+          }else if (i % 2 == 1){
+            leds[i] = CRGB::Red;
+          }
+        }
+      delay(1000);
+      }
+    if(M5.BtnB.wasPressed()){
+      num++;
     }
   }else if(num == 1){
     M5.Lcd.println("Blue  ");
@@ -58,6 +87,15 @@ void loop() {
     for(int i = 0; i < 7; i++){
       leds[i] = CRGB::Yellow;
     }
+  }else if(num == 6){
+    M5.Lcd.println("OFF   ");
+    for(int i = 0; i < 7; i++){
+      leds[i] = CRGB::Black;
+    }
   }
   FastLED.show();
+  phase++;
+  if(phase > 2){
+    phase = 0;
+  }
 }
